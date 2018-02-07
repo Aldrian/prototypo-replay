730a5b8852fe4f45cb749022c7406f68a02f0328
Aldrian <ibarretj@gmail.com>
2018-02-07 18:09:12 +0100

    fix/prototypo api environment

--- src/data/constants.js
+++ src/data/constants.js
@@ -6,9 +6,7 @@ export const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
   : 'http://localhost:3000';
 export const DEFAULT_UI_WORD = 'Hamburgefonstiv - ABC 123';
 export const GRAPHQL_API = 'https://api.graph.cool/simple/v1/cj6maa0ib2tud01656t4tp4ej';
-export const GRAPHQL_PROTOTYPO_API = process.env.NODE_ENV === 'production'
-? 'https://api.graph.cool/simple/v1/prototypo-new-dev'
-: 'https://api.graph.cool/simple/v1/prototypo';
+export const GRAPHQL_PROTOTYPO_API = 'https://api.graph.cool/simple/v1/prototypo';
 export const AUTH0_DOMAIN = 'unique-app.eu.auth0.com';
 export const AUTH0_API = 'http://localhost:3000';
 export const AUTH0_CLIENTID = 'i4KEy4mGBQIp4Yd55QC4Z1fa35qeIKN7';
